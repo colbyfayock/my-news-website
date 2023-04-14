f2d99d82f608cbd9c7e47abe935e16ecc8831f9d
Colby Fayock <fayock@gmail.com>
2023-04-13 21:43:43 -0400

    Step 3: Running a test with Applitools

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,6 +1,15 @@
 const { test, expect } = require('@playwright/test');
+import { Eyes, Target } from '@applitools/eyes-playwright';
 
 test('it should have a title', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await expect(page).toHaveTitle('The Hundred - Your antidote to information overload.');
+});
+
+test('it should load the homepage', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  const eyes = new Eyes();
+  await eyes.open(page, 'The Hundred', 'App');
+  await eyes.check(Target.window().fully())
+  await eyes.close();
 });
