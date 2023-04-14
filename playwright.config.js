f377f2fe21a037562e17fd5776cdb328457d9a48
Colby Fayock <fayock@gmail.com>
2023-04-13 21:41:19 -0400

    Step 2: Installing and configuring Applitools Eyes

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,9 @@ const { defineConfig, devices } = require('@playwright/test');
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require('dotenv').config({
+  path: './.env.local'
+});
 
 /**
  * @see https://playwright.dev/docs/test-configuration
